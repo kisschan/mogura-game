e32c08fa7202e301d46af1e3eed1cbe4f67ce709
kisschan <goldentakeshi@gmail.com>
2026-09-07 12:16:45 +0900

    chore(android): バージョンを16に更新

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.moguru.game.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "15"
+        versionCode = 16
+        versionName = "16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
